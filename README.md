7a3cefbfb154ee6ef31d8bb007ccda6ff427f986
yyna <xykaecutie@gmail.com>
2024-01-17 15:56:25 +0100

    instructions

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # 00dg
 a donation goal tracker styled like the internet of the 00s
+
+## Requirements
+1. install [python](https://www.python.org/downloads/) and [pip](https://pip.pypa.io/en/stable/installation/)
+2. install pip package `fastapi`: `pip install fastapi`
+3. install `uvicorn` or another webserver. *this tutorial will use `uvicorn`.* you can install `uvicorn` from apt or pip.
+
+## Setup
+1. clone the repository anywhere you'd like. `git clone https://github.com/yynaa/00dg`
+2. in the root of the repo, run `uvicorn run:app --port 9650`
+3. in OBS, create a new browser source
+4. set it's path to local, and set it to be `tracker.html`
+5. open `dashboard.html` to remove donation goals
+
+## Customisation - sample settings file
+```pyp
+settings = {
+    "incoming": 3, # how many incoming goals
+    "reversed": False, # if True, goals appear at the bottom
+    "api": {
+        "ws": "ws://mywebsocket.xyz",
+        "message": { "op": "sub", "msg": "donations" }, # message to send to the websocket to subscribe
+        # see script.js:socket.onmessage to edit the receiving routine
+    },
+    "goals": [
+        {
+            "goal": 420.69,
+            "description": "Goal 1",
+            "hue": 0,
+            "image": "assets/icon.png",
+            "background": "assets/background.png"
+        }
+    ]
+}
+```
+
--- settings.py
+++ settings.py
@@ -1,79 +1,17 @@
 settings = {
-    "incoming": 3,
-    "reversed": False,
+    "incoming": 3, # how many incoming goals
+    "reversed": False, # if True, goals appear at the bottom
     "api": {
-        "ws": "ws://dev.tnevent.telecomnancy.net/ws",
-        "message": { "op": 'sub', "msg": "pot" },
+        "ws": "ws://mywebsocket.xyz",
+        "message": { "op": "sub", "msg": "donations" }, # message to send to the websocket to subscribe
+        # see script.js:socket.onmessage to edit the receiving routine
     },
     "goals": [
         {
-            "goal": 100,
+            "goal": 420.69,
             "description": "Goal 1",
-            "hue": 100,
-            "image": "assets/picrew_persona.png",
-            "background": "assets/background.png"
-        },
-        {
-            "goal": 200,
-            "description": "Goal 2",
-            "hue": 200,
-            "image": "assets/picrew_persona.png",
-            "background": "assets/background.png"
-        },
-        {
-            "goal": 300,
-            "description": "Goal 3",
-            "hue": 300,
-            "image": "assets/picrew_persona.png",
-            "background": "assets/background.png"
-        },
-        {
-            "goal": 400,
-            "description": "Goal 4",
-            "hue": 400,
-            "image": "assets/picrew_persona.png",
-            "background": "assets/background.png"
-        },
-        {
-            "goal": 500,
-            "description": "Goal 5",
-            "hue": 500,
-            "image": "assets/picrew_persona.png",
-            "background": "assets/background.png"
-        },
-        {
-            "goal": 600,
-            "description": "Goal 6",
-            "hue": 600,
-            "image": "assets/picrew_persona.png",
-            "background": "assets/background.png"
-        },
-        {
-            "goal": 700,
-            "description": "Goal 7",
-            "hue": 700,
-            "image": "assets/picrew_persona.png",
-            "background": "assets/background.png"
-        },
-        {
-            "goal": 800,
-            "description": "Goal 8",
-            "hue": 800,
-            "image": "assets/picrew_persona.png",
-            "background": "assets/background.png"
-        },
-        {
-            "goal": 900,
-            "description": "Goal 9",
-            "hue": 900,
-            "image": "assets/picrew_persona.png",
-            "background": "assets/background.png"
-        },
-        {
-            "goal": 1000,
-            "description": "Goal 10",
-            "hue": 1000,
-            "image": "assets/picrew_persona.png",
+            "hue": 0,
+            "image": "assets/icon.png",
             "background": "assets/background.png"
         }
     ]
